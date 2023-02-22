804a8f3e6629c0b9f55954b5278cbbe78060fe5f
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2023-02-22 09:30:11 +0545

    feat: partial migration to vue 3 (#3)
    * feat: starting point for migration to vue 3
    
    * feat: add initial home page, navbar & footer
    
    * chore: remove telegram link
    
    * feat: add dummy views and routes
    
    * chore: delete unneeded component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <router-view />
+  <v-app>
+    <TheNavigation />
+    <v-main class="bg-grey-lighten-3">
+      <router-view />
+      <TheFooter />
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-  //
+import TheNavigation from "@/components/TheNavigation.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
---
+++ src/views/Stations.vue
@@ -0,0 +1,1 @@
+<template>Stations</template>
